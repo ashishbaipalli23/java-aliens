6fb5c71f1070f0540b51822c5293d5154f12a68d
ashishbaipalli23 <ashishbaipalli369@gmail.com>
2025-06-20 17:59:26 +0530

    List<I> and its implementation Classes committed

---
+++ Collections_In_Java/src/com/ashi/Deque_LinkedList/MainApplication.java
@@ -0,0 +1,135 @@
+package com.ashi.Deque_LinkedList;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+
+		Deque<Integer> numDeque = new LinkedList<Integer>();
+
+		// insert
+		numDeque.offer(101);
+		numDeque.offer(102);
+		numDeque.offer(103);
+		numDeque.offer(103);
+
+		numDeque.offerFirst(111);
+		numDeque.offerLast(999);
+
+		System.out.println(numDeque);
+
+		// remove
+		System.out.println(numDeque.poll()); // Retrieves and removes the head of the queue represented by this deque
+												// (in other words, the first element of this deque), or returns null if
+												// this deque is empty.
+		System.out.println(numDeque.pollFirst());// Retrieves and removes the first element of this deque, or returns
+													// null if this deque is empty.
+		System.out.println(numDeque.pollFirst());// Retrieves and removes the last element of this deque, or returns
+													// null if this deque is empty.
+
+		System.out.println(numDeque);
+
+		// peek()
+
+		System.out.println(numDeque.peek()); // Retrieves, but does not remove, the head of the queue represented by
+												// this deque (in other words, the first element of this deque), or
+												// returns null if this deque is empty.
+		System.out.println(numDeque.peekFirst()); // Retrieves, but does not remove, the first element of this deque, or
+													// returns null if this deque is empty.
+		System.out.println(numDeque.peekLast()); // Retrieves, but does not remove, the last element of this deque, or
+													// returns null if this deque is empty.
+		System.out.println(numDeque);
+
+		// pop()
+
+		System.out.println(numDeque.pop()); // Retrieves and removes the head of the queue represented by this deque
+											// (in other words, the first element of this deque), or returns null if
+											// this deque is empty.
+
+		System.out.println(numDeque);
+
+		// element()
+
+		System.out.println(numDeque.element()); // Retrieves, but does not remove, the head of the queue represented by
+												// this deque (in other words, the first element of this deque), or
+												// returns null if this deque is empty.
+
+		System.out.println(numDeque);
+
+		// remove()
+
+		System.out.println(numDeque.remove());// Retrieves and removes the head of the queue represented by this deque
+												// (in other words, the first element of this deque). This method
+												// differs from poll() only in that it throws an exception if this deque
+												// is empty.
+
+		// Deque method
+
+		Deque<Integer> numIntegers = new LinkedList<Integer>();
+		numIntegers.addAll(List.of(1, 2, 2, 3, 3, 2, 9, 3, 9));
+		System.out.println("deque :" + numIntegers);
+
+		// Removes the first occurrence of the specified element from this deque. If the
+		// deque does not contain the element, it is unchanged. More formally, removes
+		// the first element e such that Objects.equals(o, e) (if such an element
+		// exists). Returns true if this deque contained the specified element (or
+		// equivalently, if this deque changed as a result of the call).
+		System.out.println(numIntegers.removeFirstOccurrence(2));
+
+		// Removes the last occurrence of the specified element from this deque. If the
+		// deque does not contain the element, it is unchanged. More formally, removes
+		// the last element e such that Objects.equals(o, e) (if such an element
+		// exists). Returns true if this deque contained the specified element (or
+		// equivalently, if this deque changed as a result of the call).
+
+		System.out.println(numIntegers.removeLastOccurrence(3));
+		System.out.println(numIntegers);
+
+		// Returns an iterator over the elements in this deque in reverse sequential
+		// order. The elements will be returned in order from last (tail) to first
+		// (head).
+		Iterator<Integer> descendingIterator = numIntegers.descendingIterator();
+
+		while (descendingIterator.hasNext()) {
+			System.out.println(descendingIterator.next());
+		}
+
+		// add() vs offer()
+
+		Deque<Integer> numIntegers2 = new LinkedList<Integer>();
+		// Inserts the specified element into the queue represented by this deque (in
+		// other words, at the tail of this deque) if it is possible to do so
+		// immediately without violating capacity restrictions, returning true upon
+		// success and throwing an IllegalStateException if no space is currently
+		// available.
+		numIntegers2.add(1);
+
+		// Inserts the specified element into the queue represented by this deque (in
+		// other words, at the tail of this deque) if it is possible to do so
+		// immediately without violating capacity restrictions, returning true upon
+		// success and false if no space is currently available
+		numIntegers2.offer(2);
+
+		numIntegers2.addFirst(3);
+		numIntegers2.offerFirst(4);
+		numIntegers2.addLast(5);
+		numIntegers2.offerLast(6);
+
+		System.out.println(numIntegers2);
+
+		// push()
+//		Pushes an element onto the stack represented by this deque (in other words, at the head of this deque) if it is possible to do so immediately without violating capacity restrictions, throwing an IllegalStateException if no space is currently available.
+//		This method is equivalent to addFirst().
+
+		numIntegers2.push(1111);
+		System.out.println(numIntegers2);
+		
+		
+     
+	}
+
+}
---
+++ Collections_In_Java/src/com/ashi/Vector_Class/MainApplication.java
@@ -0,0 +1,83 @@
+package com.ashi.Vector_Class;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Vector;
+
+public class MainApplication {
+
+	 public static void main(String[] args) {
+		
+		 
+		 Vector<Integer> numIntegers = new Vector<Integer>();
+		 numIntegers.add(10);
+		 numIntegers.add(11);
+		 numIntegers.add(12);
+		 
+		 System.out.println(numIntegers);
+		 
+		 Enumeration<Integer> elements = numIntegers.elements();
+		 while (elements.hasMoreElements()) {
+			System.out.println(elements.nextElement());
+	
+		}
+		 //new method interduced in java 9 in Enumeration<I>
+		 //asItertor()
+		 Enumeration<Integer> elements1 = numIntegers.elements();
+		 Iterator<Integer> asIterator = elements1.asIterator();
+		 while (asIterator.hasNext()) {
+			System.out.println(asIterator.next());
+			
+		}
+		 
+		 //elementAt()
+		 Integer elementAt = numIntegers.elementAt(1);
+		 System.out.println("element at index 1: "+elementAt);
+		 
+		
+		 System.out.println("size : "+numIntegers.size());
+		 System.out.println("capacity : "+numIntegers.capacity());
+		 
+		 numIntegers.ensureCapacity(20);
+		 System.out.println("ensureCapacity(20); capacity : "+numIntegers.capacity());
+		 
+		 
+		 numIntegers.trimToSize(); //size() will come here
+		 System.out.println(" numIntegers.trimToSize() => capacity : "+numIntegers.capacity());
+		 
+		
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
